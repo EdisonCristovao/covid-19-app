fb06b01136317f237e9e6d91044b173235a7f77b
Edison <edison.cr.jr@gmail.com>
2020-12-07 22:04:27 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:covidapp/constants.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Covid 19',
+      debugShowCheckedModeBanner: false,
+      // debugShowMaterialGrid: true,
+      theme: ThemeData(
+          scaffoldBackgroundColor: kBackgroundColor,
+          fontFamily: "Poppins",
+          textTheme: TextTheme(
+            bodyText1: TextStyle(color: kBodyTextColor),
+          )),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 350,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Color(0xFF3383CD),
+                  Color(0xFF11249F),
+                ],
+              ),
+              image: DecorationImage(
+                image: AssetImage("assets/images/virus.png"),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
